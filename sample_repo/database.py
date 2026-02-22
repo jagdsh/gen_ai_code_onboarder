cf5dd32f7a0126573e3ec5ae9ea7334e84fd82e0
Jags <jagdsh.chand-ext@guccidigital.com>
2026-02-22 09:50:45 +0100

    adding first commit for assignment

---
+++ sample_repo/database.py
@@ -0,0 +1,9 @@
+
+def connect_to_db(url):
+    '''Connects to the postgres database.'''
+    print(f"Connecting to {url}...")
+    return True
+
+def fetch_users():
+    '''Fetches all active users from the database.'''
+    return ["Alice", "Bob"]
